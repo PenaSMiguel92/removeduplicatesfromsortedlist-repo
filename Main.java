53adb602eb5d5d7b0d9b028b9ca9d429e29fffb3
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-17 15:24:04 -0700

    added generateListFromValues method to Main class

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Solution sol = new Solution();
+
+    }
+
+    public ListNode generateListFromValues(int[] values) {
+        ListNode head = new ListNode(-1, null);
+        ListNode pointer = head;
+        for (int val : values) {
+            pointer.next = new ListNode(val);
+            pointer = pointer.next;
+        }
+        return head.next;
     }
 }
