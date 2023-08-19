4d1f853d10cb155230e29d96e56756f9851fc281
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-19 13:48:44 -0700

    main functionality

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
+        ListNode list1 = generateListFromValues(new int[] { 1, 1, 2 });
+        ListNode list2 = generateListFromValues(new int[] { 1, 1, 2, 3, 3 });
+        ListNode list3 = generateListFromValues(new int[] { -30, -230, 20, 140, 500 });
 
+        printList(sol.deleteDuplicates(list1));
+        printList(sol.deleteDuplicates(list2));
+        printList(sol.deleteDuplicates(list3));
     }
 
-    public ListNode generateListFromValues(int[] values) {
+    public static ListNode generateListFromValues(int[] values) {
         ListNode head = new ListNode(-1, null);
         ListNode pointer = head;
         for (int val : values) {
@@ -12,5 +18,14 @@ public class Main {
             pointer = pointer.next;
         }
         return head.next;
+    }
+
+    public static void printList(ListNode list) {
+        String val = "[";
+        while (list != null) {
+            val += list.val;
+            list = list.next;
+        }
+        System.out.println(val + "]");
     }
 }
